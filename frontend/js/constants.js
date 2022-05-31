fca833565718a4eeded30e06601b890ab2365a96
Sasukeeey <olim123q@gmail.com>
2022-05-31 14:29:04 +0200

    italinao

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,7 +1,7 @@
 const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the Potato Mafia!!";
+const welcome_h1 = "Bonasera Bonasera *italian noises*";
 const welcome_h2 = "Connect to MetaMask to Get Started";
 const welcome_p = '';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
